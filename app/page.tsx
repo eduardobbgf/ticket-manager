b29f0edc49ca4a9a36374863706bccb38eabf656
eduardobbgf <eduardo.bbgf@gmail.com>
2024-03-27 14:02:37 -0300

    chore: create a generic

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      Teste
+    <main className="p-14 w-full bg-white">
+      <h3 className="font-extralight text-2xl mb-4">Welcome</h3>
+      <span className="font-light text-md">
+        Here you can create ticket for issues in your VTEX app.
+      </span>
     </main>
   );
 }
